06cbf8415d862c0a8045c26fe1cb528f6dfce721
samuelsson87 <samuelsson87@hotmail.com>
2017-03-30 11:17:07 +0200

    lab1 uppdaterad

--- Mobilutveckling/lab1/controller.js
+++ Mobilutveckling/lab1/controller.js
@@ -3,25 +3,12 @@ var marker;
 var chicago = {lat: 41.85, lng: -87.65};
 var kth = {lat: 59.347540, lng: 18.0738};
 var pos2 = {lat: 59.347340, lng: 18.073044};
+var humle = {lat:59.339282, lng: 18.072832};
+var erlands = {lat:59.340487 , lng:18.040420};
+var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
 
 
-
-//Toggle tilt between 0 and 45 degrees
-function setTilt(){
-  // console.log('hej')
-
-  if (map.tilt==0){
-      //Shorthand if statement - Zoom in for tilt if neccesary
-    map.zoom < 19? map.setZoom(19) : '' ;
-    map.setTilt(45);
-  }
-
-  else{
-    map.setTilt(0);
-  }
-}
-
-
+//Basic navigation
 function moveUp(){
   map.setCenter({lat:map.getCenter().lat()+0.003, lng:map.getCenter().lng()});
   //console.log((1/(*map.getZoom())*0.05));
@@ -40,7 +27,6 @@ function moveRight(){
 }
 
 
-
 function zoomIn(){
   map.setZoom(map.getZoom() + 1);
 }
@@ -50,12 +36,48 @@ function zoomOut(){
 }
 
 
+//Move center of map
+function moveCenter(position) {
+    map.setCenter(position);
+  };
 
+//Show current position
+function showLocation(){
+   if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(function (position) {
+         initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+         map.setCenter(initialLocation);
+
+        newMarker = new google.maps.Marker({
+        position: initialLocation,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: "You're here!"
+      });
+    });
+  }
+}
+
+
+//Toggle tilt between 0 and 45 degrees
+function setTilt(){
+
+  if (map.tilt==0){
+    //Shorthand if statement - Zoom in for tilt if neccesary
+    map.zoom < 19? map.setZoom(19) : '' ;
+    map.setMapTypeId('satellite');
+    map.setTilt(45);
+  }
+
+  else{
+    map.setTilt(0);
+    map.setMapTypeId('roadmap');
+  }
+}
+
+
+//Make marker bounce
 function toggleBounce() {
-  // console.log("bounce function running");
-  // console.log(marker);
-  // marker.setAnimation(google.maps.Animation.BOUNCE);
-
   if (marker.getAnimation() !== null) {
     marker.setAnimation(null);
   } else {
@@ -65,7 +87,7 @@ function toggleBounce() {
 
 
 
-
+//Google API-buttons
 /**
  * The CenterControl adds a control to the map that recenters the map on
  * Chicago.
@@ -73,36 +95,36 @@ function toggleBounce() {
  * @constructor
  */
 
-function CenterControl(controlDiv, map) {
+// function CenterControl(controlDiv, map) {
 
-  // Set CSS for the control border.
-  var controlUI = document.createElement('div');
-  controlUI.style.backgroundColor = '#fff';
-  controlUI.style.border = '2px solid #fff';
-  controlUI.style.borderRadius = '3px';
-  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-  controlUI.style.cursor = 'pointer';
-  controlUI.style.marginBottom = '22px';
-  controlUI.style.textAlign = 'center';
-  controlUI.title = 'Click to recenter the map';
-  controlDiv.appendChild(controlUI);
+//   // Set CSS for the control border.
+//   var controlUI = document.createElement('div');
+//   controlUI.style.backgroundColor = '#fff';
+//   controlUI.style.border = '2px solid #fff';
+//   controlUI.style.borderRadius = '3px';
+//   controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+//   controlUI.style.cursor = 'pointer';
+//   controlUI.style.marginBottom = '22px';
+//   controlUI.style.textAlign = 'center';
+//   controlUI.title = 'Click to recenter the map';
+//   controlDiv.appendChild(controlUI);
 
-  // Set CSS for the control interior.
-  var controlText = document.createElement('div');
-  controlText.style.color = 'rgb(25,25,25)';
-  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-  controlText.style.fontSize = '16px';
-  controlText.style.lineHeight = '38px';
-  controlText.style.paddingLeft = '5px';
-  controlText.style.paddingRight = '5px';
-  controlText.innerHTML = 'Center Map';
-  controlUI.appendChild(controlText);
+//   // Set CSS for the control interior.
+//   var controlText = document.createElement('div');
+//   controlText.style.color = 'rgb(25,25,25)';
+//   controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+//   controlText.style.fontSize = '16px';
+//   controlText.style.lineHeight = '38px';
+//   controlText.style.paddingLeft = '5px';
+//   controlText.style.paddingRight = '5px';
+//   controlText.innerHTML = 'Center Map';
+//   controlUI.appendChild(controlText);
 
-  // Setup the click event listeners: simply set the map to Chicago.
-  controlUI.addEventListener('click', function() {
-    map.setCenter(chicago);
-  });
-}
+//   // Setup the click event listeners: simply set the map to Chicago.
+//   controlUI.addEventListener('click', function() {
+//     map.setCenter(chicago);
+//   });
+// }
 
 
 
@@ -136,7 +158,7 @@ function initMap() {
     position: kth,
     map: map,
     animation: google.maps.Animation.DROP,
-    title: "I'm stuck here"
+    title: "Drag or Bounce me!"
   });
 
   marker2 = new google.maps.Marker({
@@ -144,20 +166,35 @@ function initMap() {
     map: map,
     draggable: true,
     animation: google.maps.Animation.DROP,
-    title: 'Drag or Bounce me!'
+    title: 'Move me!',
+    icon: iconBase + 'info-i_maps.png'
   });
+
+  marker3 = new google.maps.Marker({
+    position: humle,
+    map: map,
+    title: "Humlegården"
+  });
+
+  marker4 = new google.maps.Marker({
+    position: erlands,
+    map: map,
+    title: "Erlands Bar"
+  });
+
   marker.addListener('click', toggleBounce);
 
-
+  //Google API BUTTONS
   // Create the DIV to hold the control and call the CenterControl()
   // constructor passing in this DIV.
-  var centerControlDiv = document.createElement('div');
-  var centerControl = new CenterControl(centerControlDiv, map);
+  // var centerControlDiv = document.createElement('div');
+  // var centerControl = new CenterControl(centerControlDiv, map);
 
-  centerControlDiv.index = 1;
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
+  // centerControlDiv.index = 1;
+  // map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
 
 
+  //For flashscreen-ish
   setTimeout(displayContent,2000);
 
 }
