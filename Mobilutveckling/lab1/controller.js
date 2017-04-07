47c0b3b712f350d6a90b00247cc172a9639917ad
samuelsson87 <samuelsson87@hotmail.com>
2017-04-07 09:25:40 +0200

    uppdaterad lab1

--- Mobilutveckling/lab1/controller.js
+++ Mobilutveckling/lab1/controller.js
@@ -7,6 +7,10 @@ var humle = {lat:59.339282, lng: 18.072832};
 var erlands = {lat:59.340487 , lng:18.040420};
 var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
 
+$('#navigation').hide();
+$('#placesMenu').hide();
+$('#settingsMenu').hide();
+
 
 //Basic navigation
 function moveUp(){
@@ -86,55 +90,14 @@ function toggleBounce() {
 }
 
 
-//Google API-buttons
-/**
- * The CenterControl adds a control to the map that recenters the map on
- * Chicago.
- * This constructor takes the control DIV as an argument.
- * @constructor
- */
-
-// function CenterControl(controlDiv, map) {
-
-//   // Set CSS for the control border.
-//   var controlUI = document.createElement('div');
-//   controlUI.style.backgroundColor = '#fff';
-//   controlUI.style.border = '2px solid #fff';
-//   controlUI.style.borderRadius = '3px';
-//   controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-//   controlUI.style.cursor = 'pointer';
-//   controlUI.style.marginBottom = '22px';
-//   controlUI.style.textAlign = 'center';
-//   controlUI.title = 'Click to recenter the map';
-//   controlDiv.appendChild(controlUI);
-
-//   // Set CSS for the control interior.
-//   var controlText = document.createElement('div');
-//   controlText.style.color = 'rgb(25,25,25)';
-//   controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-//   controlText.style.fontSize = '16px';
-//   controlText.style.lineHeight = '38px';
-//   controlText.style.paddingLeft = '5px';
-//   controlText.style.paddingRight = '5px';
-//   controlText.innerHTML = 'Center Map';
-//   controlUI.appendChild(controlText);
-
-//   // Setup the click event listeners: simply set the map to Chicago.
-//   controlUI.addEventListener('click', function() {
-//     map.setCenter(chicago);
-//   });
-// }
-
-
-
 function displayContent(){ 
   document.getElementById("loading").remove()
 
-    if (window.navigator.standalone) {
-    // document.getElementById("prompt").addClass("hide")
-  } else {
+  if (window.navigator.standalone) {
+  } 
+
+  else {
     setTimeout(function(){confirm('Consider adding this web app to your homescreen!')}, 2000);
-    // document.getElementById("prompt").addClass("alert")
   } 
 
   document.getElementById("mainContent").style.visibility = 'visible';
@@ -217,17 +180,26 @@ function initMap() {
     infowindow2.open(map, marker3);
   });
 
-  //Google API BUTTONS
-  // Create the DIV to hold the control and call the CenterControl()
-  // constructor passing in this DIV.
-  // var centerControlDiv = document.createElement('div');
-  // var centerControl = new CenterControl(centerControlDiv, map);
-
-  // centerControlDiv.index = 1;
-  // map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
-
 
   //For flashscreen-ish
   setTimeout(displayContent,2000);
 
 }
+
+
+//Interaction
+$('#navigate').click(function(){
+  $('#navigation').toggle();
+})
+
+$('#places').click(function(){
+  $('#placesMenu').toggle();
+})
+
+$('#settings').click(function(){
+  $('#settingsMenu').toggle();
+})
+
+$('.placeButton').click(function(){
+  $('#placesMenu').hide();
+})
